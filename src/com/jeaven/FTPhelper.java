27639ddfb11f7e2dbcd79a8ba1d109f33861016e
jeavenwong <jeavenwong@163.com>
2019-09-01 11:36:19 +0800

    update

--- src/com/jeaven/FTPhelper.java
+++ src/com/jeaven/FTPhelper.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.net.SocketException;
 
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 
 public class FTPhelper {
 	//定义ftp的参数
@@ -50,6 +51,8 @@ public class FTPhelper {
 			ftpClient.enterLocalPassiveMode();	//采取被动传输模式
 			ftpClient.setDataTimeout(defaultTimeout); 	//设置默认传输超时时间
 			ftpClient.setBufferSize(1024*1024);	//设置缓冲区1M
+			//判断是否存在指定的文件路径
+			if(!JudgeHasFileDir(ftpClient, savedPath)) return "不存在指定的文件夹路径";
 			//切换工作目录到保存的路径下面，如果不存在就创建一个目录
 			boolean isFileDirExisted = ftpClient.changeWorkingDirectory(savedPath);
 			if(isFileDirExisted == false) {   //如果不存在就创造一个路径，切换工作目录
@@ -76,10 +79,63 @@ public class FTPhelper {
 		}
 	}
 	
-	//Test
+	
+	/*
+	 * 判断是否 存在指定的文件夹
+	 * @param fileDirName 文件夹名字
+	 * */
+	public boolean JudgeHasFileDir(FTPClient ftpClient, String FileDirName) {
+		try {
+			FTPFile[] ftpFiles = ftpClient.listFiles();
+			for(FTPFile file : ftpFiles) {
+				if(file.isDirectory() && file.getName().equals(FileDirName)) {
+					return true;
+				} else {
+					continue;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	
+	/*
+	 * test
+	 * */
 	public static void main(String[] args) {
+		String fileName = "";  //本地完整的文件绝对路径
+		String savedPath = "";  //ftp保存的文件夹
+		String savedFileName = ""; //ftp保存的文件的名字
+		//命令行初始化参数
+		if(args.length > 0) {
+			if(args[0] != null) {
+				 fileName = args[0];
+			} else {
+				System.out.println("缺少参数  请输入参数  fileName");
+				return;
+			}
+			if(args[1] != null) {
+				savedPath = args[1];
+			} else {
+				System.out.println("缺少参数 请输入参数  savedPath");
+				return;
+			}
+			if(args[2] != null) {
+				savedFileName = args[2];
+			} else {
+				System.out.println("缺少参数 请输入参数  savedFileName");
+				return;
+			}
+		} else {
+			System.out.println("请输入指定的参数 fileName savedPath savedFileName");
+			return;
+		}
+		
+		//建立ftp客户端
 		FTPhelper ftPhelper = new FTPhelper();
-		File file = new File("C:/Users/jeave/Desktop/test1.txt");
+		File file = new File(fileName);
 		if(!file.exists()) {
 			try {
 				file.createNewFile();
@@ -87,8 +143,7 @@ public class FTPhelper {
 				e.printStackTrace();
 			}
 		}
-		String savedFileName = "bbb.txt"; 
-		String status = ftPhelper.uploadFile("test", file, savedFileName);
+		String status = ftPhelper.uploadFile(savedPath, file, savedFileName);
 		System.out.println(status);
 	}
 	
